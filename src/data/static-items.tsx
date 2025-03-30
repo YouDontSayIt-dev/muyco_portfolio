ee277c66d4130c9eef037478e7771394d61f6e93
YouDontSayIt-dev <mjmuyco13@gmail.com>
2025-03-31 03:28:29 +0800

    [FIX] styling changes on experience page

--- src/data/static-items.tsx
+++ src/data/static-items.tsx
@@ -86,8 +86,8 @@ export const data = [
         <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-md font-normal mb-8">
           C# Developer | Jul 2024 - Present
         </p>
-        <div className="grid grid-cols-2 gap-2 text-justify">
-        Before graduating, I worked as a Full-Stack Developer at Golden Suntec, where I gained hands-on experience in enterprise development, stored procedures, and the .NET environment.
+        <div className="grid xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 grid-cols-1 gap-2 text-justify">
+        I worked as a Full-Stack Developer at Golden Suntec, where I gained hands-on experience in enterprise development, stored procedures, and the .NET environment.
         <br/>During my time here, I learned invaluable lessons from my senior mentors, who introduced me to best coding practices for writing readable, maintainable, and scalable code. This experience significantly shaped my approach to software development and problem-solving.
         </div>
       </div>
@@ -103,7 +103,7 @@ export const data = [
         <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-md font-normal mb-8">
           C# Game Developer | Aug 2023 - Sep 2023
         </p>
-        <div className="grid grid-cols-2 gap-2 mb-4 leading-auto text-justify">
+        <div className="grid  xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 grid-cols-1  gap-2 mb-4 leading-auto text-justify">
             At Lamina, I gained first-hand experience in production-level development using Unity Engine. It was here that I learned how to effectively collaborate with diverse individuals, leveraging our strengths and weaknesses to build innovative solutions.
         </div>
         <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-lg font-normal mb-2">
@@ -112,7 +112,7 @@ export const data = [
         <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-md font-normal mb-8">
           Freelance Software Developer | Jul 2023 - Mar 2024
         </p>
-        <div className="grid grid-cols-2 gap-2 text-justify">
+        <div className="grid xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 grid-cols-1  gap-2 text-justify">
             As a freelance software developer, I built AI-based systems, as well as reservation and tracking solutions, tailored to client needs.  
             Utilizing PHP, Python, and Java, I developed scalable and efficient applications that streamlined operations and enhanced user experience.
         </div>
